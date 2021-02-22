70bd45baf6bf603d4f59052c3bb8d65173519182
Adan Escamilla <adanescamilla580@gmail.com>
2021-02-21 21:34:59 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your code here!
+
+
+var el = document.getElementById('main')
+
+el.remove();
+
+const newHeader = document.createElement("h1");
+
+
+const newContent = document.createTextNode("Adan is the champion!");
+newHeader.appendChild(newContent);
+
+
+newHeader.setAttribute('id','victory');
+
